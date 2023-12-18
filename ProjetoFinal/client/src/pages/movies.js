a9a5d3ba35ac6335b3116bd11c68bdb387bbd805
Afonso <a75660@ualg.pt>
2023-12-18 09:50:03 +0000

    Search bar adicionada

--- ProjetoFinal/client/src/pages/movies.js
+++ ProjetoFinal/client/src/pages/movies.js
@@ -6,11 +6,21 @@ const API_BASE = "http://localhost:8080";
 
 function Movies() {
 	const navigate = useNavigate();
-    const [movies, setMovies] = useState([]);
+	const [movies, setMovies] = useState([]);
+	const [searchTerm, setSearchTerm] = useState("");
+	const [filteredMovies, setFilteredMovies] = useState([]);
 
 	useEffect(() => {
 		getMovies();
 	}, []);
+
+	useEffect(() => {
+		// Filter movies based on the search term
+		const filtered = movies.filter(movie =>
+			movie.title.toLowerCase().includes(searchTerm.toLowerCase())
+		);
+		setFilteredMovies(filtered);
+	}, [searchTerm, movies]);
 
 	const getMovies = async () => {
 		try {
@@ -28,9 +38,18 @@ function Movies() {
 		<div className="App">
 			<button onClick={() => navigate("/")}>Home</button>
 			<button onClick={() => navigate("/tvShows")}>tvShows</button>
+
+			{/* Search bar */}
+			<input
+				type="text"
+				placeholder="Search by name..."
+				value={searchTerm}
+				onChange={(e) => setSearchTerm(e.target.value)}
+			/>
+
 			<div className="container">
 				<div className="row">
-					{movies.map((movie) => (
+					{filteredMovies.map((movie) => (
 						<div className="col-sm-3" key={movie._id}>
 							<MovieCard movie={movie} />
 						</div>
--- ProjetoFinal/client/src/pages/tvShows.js
+++ ProjetoFinal/client/src/pages/tvShows.js
@@ -7,10 +7,22 @@ const API_BASE = "http://localhost:8080";
 function TvShows() {
 	const navigate = useNavigate();
 	const [tvShows, setTvShows] = useState([]);
+	const [searchTerm, setSearchTerm] = useState("");
+	const [filteredTvShows, setFilteredTvShows] = useState([]);
 
 	useEffect(() => {
 		getTvShows();
 	}, []);
+
+	useEffect(() => {
+		// Filter tvshows based on the search term
+		const filtered = tvShows.filter(tvShow =>
+			tvShow.title.toLowerCase().includes(searchTerm.toLowerCase())
+		);
+		setFilteredTvShows(filtered);
+	}, [searchTerm, tvShows]);
+
+	
 
 	const getTvShows = async () => {
 		try {
@@ -28,9 +40,16 @@ function TvShows() {
 		<div className="App">
 			<button onClick={() => navigate("/")}>Home</button>
 			<button onClick={() => navigate("/movies")}>Movies</button>
+			{/* Search bar */}
+			<input
+				type="text"
+				placeholder="Search by name..."
+				value={searchTerm}
+				onChange={(e) => setSearchTerm(e.target.value)}
+			/>
 			<div className="container">
 				<div className="row">
-					{tvShows.map((tvShow) => (
+					{filteredTvShows.map((tvShow) => (
 						<div className="col-sm-3" key={tvShow._id}>
 							<TvShowCard tvShow={tvShow} />
 						</div>
